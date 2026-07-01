4443b2bdad3a5d67152861898f5d67f5b39eb4bc
PradoRuhan <ruhan-prado@hotmail.com>
2026-07-01 18:33:30 -0300

    Sprint 2 - Tela de Login desenvolvida com Bootstrap e Reactive Forms

--- src/app/pages/login/login.ts
+++ src/app/pages/login/login.ts
@@ -1,12 +1,39 @@
 import { Component } from '@angular/core';
+import {
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators
+} from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './login.html',
   styleUrl: './login.css'
 })
 export class Login {
+
+  loginForm: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+
+    this.loginForm = this.fb.group({
+
+      username: ['', Validators.required],
+
+      password: ['', Validators.required]
+
+    });
+
+  }
+
+  onLogin(): void {
+
+    console.log('Dados do formulário:');
+    console.log(this.loginForm.value);
+
+  }
 
 }
