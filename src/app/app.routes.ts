c4bbd9f468ef2663ef906217c4204e53b8fe4b02
PradoRuhan <ruhan-prado@hotmail.com>
2026-07-01 17:01:52 -0300

    Initial commit - Estrutura inicial do projeto

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,35 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { Login } from './pages/login/login';
+import { Home } from './pages/home/home';
+import { Dashboard } from './pages/dashboard/dashboard';
+
+export const routes: Routes = [
+
+  {
+    path: '',
+    redirectTo: 'login',
+    pathMatch: 'full'
+  },
+
+  {
+    path: 'login',
+    component: Login
+  },
+
+  {
+    path: 'home',
+    component: Home
+  },
+
+  {
+    path: 'dashboard',
+    component: Dashboard
+  },
+
+  {
+    path: '**',
+    redirectTo: 'login'
+  }
+
+];
