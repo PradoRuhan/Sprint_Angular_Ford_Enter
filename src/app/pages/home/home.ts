1f517104112bdc6bd357bd49071c8a2f692c5969
PradoRuhan <ruhan-prado@hotmail.com>
2026-07-01 19:06:33 -0300

    Sprint 3 - Estrutura da Home e menu lateral

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -1,12 +1,23 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { Header } from '../../shared/header/header';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [
+    RouterLink,
+    Header
+  ],
   templateUrl: './home.html',
   styleUrl: './home.css'
 })
 export class Home {
+
+  menuAberto = false;
+
+  alternarMenu() {
+    this.menuAberto = !this.menuAberto;
+  }
 
 }
